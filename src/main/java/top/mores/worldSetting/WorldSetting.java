ef1036a14c93f4f118a88f30dce02dce0656d625
Do0oMores <87219087+Do0oMores@users.noreply.github.com>
2025-11-23 00:00:39 +0800

    :rocket: Create

---
+++ src/main/java/top/mores/worldSetting/WorldSetting.java
@@ -0,0 +1,17 @@
+package top.mores.worldSetting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldSetting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
